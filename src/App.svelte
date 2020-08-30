769f5344f88338e7158373d9f0b4146bf6e0e6ba
Godfrey Best <godfrey.c.best@gmail.com>
2020-08-30 11:26:05 -0400

    button for dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
   import Tailwind from './Tailwind.svelte';
+
+  import Button from './components/Button.svelte';
+
+  // var d = document.documentElement,
+  //   t = document.querySelector('#theme-btn'),
+  //   m = localStorage.getItem('theme');
+
+  // if (m == 'dark') {
+  //   d.classList.add('theme-dark');
+  // }
+
+  // t.addEventListener('click', function () {
+  //   if (d.classList.contains('theme-dark')) {
+  //     d.classList.remove('theme-dark');
+  //     localStorage.removeItem('theme');
+  //   } else {
+  //     d.classList.add('theme-dark');
+  //     localStorage.setItem('theme', 'dark');
+  //   }
+  // });
 </script>
 
 <style lang="scss">
@@ -8,9 +28,21 @@
       font-size: 2rem;
     }
   }
+
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0084f6;
+    transition: background-color 0.3s;
+  }
+  :global(body.dark-mode) {
+    background-color: #1d3040;
+    color: #bfc2c7;
+  }
 </style>
 
 <Tailwind />
 <main>
   <h1 class="text-red-400">The rest of our app will be here</h1>
+  <Button>Toggle</Button>
+
 </main>
