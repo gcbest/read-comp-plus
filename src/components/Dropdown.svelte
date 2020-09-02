cdbf2d484a8a0bb96ad727300e41fb826068b129
Godfrey Best <godfrey.c.best@gmail.com>
2020-09-01 22:22:07 -0400

    add tailwind config

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -9,8 +9,8 @@
     z-index: 2;
   }
 
-  ul a {
-    @apply bg-gray-200 py-2 px-4 block whitespace-no-wrap border-solid;
+  ul li:not(:first-child):not(:last-child) {
+    @apply bg-gray-200 py-2 px-4 block whitespace-no-wrap border-solid border-gray-500 border-t border-b;
   }
 
   ul li:first-child a {
@@ -25,15 +25,12 @@
 <div class="dropdown inline-block relative">
   <button
     class="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex
-    items-center">
+      items-center">
     <span class="mr-1">{title}</span>
   </button>
   <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
     {#each options as opt}
-      <li class="">
-        <a href="#">{opt}</a>
-      </li>
+      <li class="border-solid"><a href="#">{opt}</a></li>
     {/each}
-
   </ul>
 </div>
