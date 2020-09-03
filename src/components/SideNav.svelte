7a29af75e12d4462b62b426baff2c702387ba7ad
Godfrey Best <godfrey.c.best@gmail.com>
2020-09-02 21:55:20 -0400

    add toggle dark mode to sidenav on mobile

--- src/components/SideNav.svelte
+++ src/components/SideNav.svelte
@@ -1,5 +1,6 @@
 <script>
   import Dropdown from './Dropdown.svelte';
+  import ToggleDarkMode from './ToggleDarkMode.svelte';
 
   const closeNav = () => {
     document.getElementById('mySidenav').style.width = '0';
@@ -24,7 +25,7 @@
   }
 
   .sidenav span {
-    padding: 8px 8px 8px 32px;
+    padding: 1rem;
     text-decoration: none;
     font-size: 25px;
     /* color: #818181; */
@@ -44,6 +45,10 @@
     margin-left: 50px;
   }
 
+  .sidenav .toggle-dark-mode {
+    display: none;
+  }
+
   /* DARK THEME */
   :global(body.dark-mode) .sidenav {
     background-color: #457b9d;
@@ -51,12 +56,20 @@
     /* @apply text-red-400; */
   }
 
-  @media screen and (max-height: 600px) {
+  @media screen and (max-width: 600px) {
     .sidenav {
       padding-top: 15px;
     }
     .sidenav a {
       font-size: 18px;
+    }
+
+    .sidenav .toggle-dark-mode {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      padding: 1.5rem;
     }
   }
 </style>
@@ -65,8 +78,10 @@
   <nav>
     <ul>
       <li>
+        <span class="toggle-dark-mode">
+          <ToggleDarkMode />
+        </span>
         <span class="closebtn cursor-pointer" on:click={closeNav}>&times;</span>
-
       </li>
       <li>
         <Dropdown title="Speed" options={speedOptions} />
