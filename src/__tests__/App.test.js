980910bb6b66709b5fef8c1a84449170b903ab04
Godfrey Best <godfrey.c.best@gmail.com>
2020-09-15 21:41:46 -0400

    remove gettestbyid

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,7 +3,6 @@ const App = require('../App.svelte');
 
 describe('yerr', () => {
         const { getByText, getByTestId } = render(App);
-        const main = getByTestId('man');
 
         it('should do it', () => {
                 const num1 = 2;
